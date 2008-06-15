8a1d704102e98e537dffedc33141bcd101e0a67e
Brendon Hogger <brendonh@gmail.com>
2008-06-15 15:06:19 +0000

    Table views
    git-svn-id: http://dev.brendonh.org/svn/evo/trunk@34 b2fba486-9eff-4e6f-b9e8-fc05d33d65a9

--- evo/src/evopgsql.erl
+++ evo/src/evopgsql.erl
@@ -47,14 +47,20 @@ request(Req) ->
             Req:respond({500, [], <<"Internal error">>})
     end.
 
-dispatch(Req, 'GET', ["tables"]) ->
+dispatch(_Req, 'GET', ["tables"]) ->
     Tables = gen_server:call(magicdb, {getTables, "routecomplete"}),
     Data = [{tables, lists:sort(Tables)}, {pretty, fun pretty_name/1}],
     {ok, Result} = gen_server:call(evotemplate, {run, tableList, Data, run_raw}),
     {ok, "text/html", "Tables", Result};
 
-dispatch(Req, 'GET', ["model",ID]) ->
-    {ok, "text/html", "Model", list_to_binary(lists:flatten(io_lib:format("Model ~p", [ID])))};
+dispatch(_Req, 'GET', ["tables",Name]) ->
+    Table = list_to_atom(Name),
+    Columns = gen_server:call(magicdb, {getColumns, Table}),
+    ColNames = lists:map(fun({K,_V}) -> K end, Columns),
+    Rows = gen_server:call(magicdb, {getRows, Table, []}),
+    Data = [{columns, ColNames}, {rows, Rows}, {db_value, fun format_db_value/1}],
+    {ok, Result} = gen_server:call(evotemplate, {run, tableContent, Data, run_raw}),
+    {ok, "text/html", Name, Result};
 
 dispatch(Req, 'GET', [""]) ->
     {other, Req:respond({302, [{<<"Location">>, <<"/tables">>}], <<"">>})};
@@ -70,6 +76,14 @@ pretty_name(Name) ->
     Bits = string:tokens(Name, "_"),
     TitleBits = lists:map(fun([C|R]) -> [string:to_upper(C)|R] end, Bits),
     string:join(TitleBits, " ").
+
+
+format_db_value({_Col, null}) ->
+    {tags, [{"<div class=\"null\">", "null", "</div>"}]};
+format_db_value({_Col, Val}) when is_integer(Val) ->
+    {tags, [{"<div class=\"number\">", integer_to_list(Val), "</div>"}]};
+format_db_value({_Col, Val}) ->
+    Val.
 
 
 template(site) -> "
@@ -98,4 +112,23 @@ template(tableList) -> atom_to_list('
     <a><e:attr name="href" e:render="data">/tables/</e:attr><e:slot e:format="pretty" /></a>
   </li>
 </ul>
-').
+');
+
+template(tableContent) -> "
+<div class=\"tableScroll\">
+<table class=\"dbTable\">
+  <tr e:key=\"columns\">
+    <th class=\"first\" e:render=\"data\" e:dataExp=\"lists:nth(1,D)\" />
+    <e:inv e:render=\"foreach\" e:dataExp=\"lists:nthtail(1,D)\"><th e:render=\"data\" /></e:inv>
+  </tr>
+  <tbody e:render=\"foreach\" e:key=\"rows\">
+    <tr>
+      <e:attr name=\"class\" e:render=\"data\" e:dataExp=\"OddEven\" />
+      <td class=\"first\" e:render=\"data\" e:format=\"db_value\" e:dataExp=\"lists:nth(1,D)\" />
+      <e:inv e:render=\"foreach\" e:dataExp=\"lists:nthtail(1,D)\"><td e:render=\"data\" e:format=\"db_value\" /></e:inv>
+    </tr>
+  </tbody>
+</table>
+</div>
+".
+
